8aabef96318e6f7d3e850d58c0610a29d788f246
Sanjin888 <sanjinkurtalija@gmail.com>
2023-06-21 22:18:54 +0200

    removed admin request

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,35 +3,12 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['aldin.kovacevic@ibu.edu.ba ','edvin955@gmail.com','edvin.halilovic@stu.ibu.edu.ba','nermina.durmic@ibu.edu.ba','sanjin.kurtalija@stu.ibu.edu.ba', 'sanjinkurtalija@gmail.com'];
-
-export const authOptions = {
-  secret: process.env.SECRET,
-  providers: [
+export default NextAuth({
+  providers:[
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-  callbacks: {
-    session: ({session,token,user}) => {
-      if (adminEmails.includes(session?.user?.email)) {
-        return session;
-      } else {
-        return false;
-      }
-    },
-  },
-};
-
-export default NextAuth(authOptions);
-
-export async function isAdminRequest(req,res) {
-  const session = await getServerSession(req,res,authOptions);
-  if (!adminEmails.includes(session?.user?.email)) {
-    res.status(401);
-    res.end();
-    throw 'not an admin';
-  }
-}
+})
